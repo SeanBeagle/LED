9094ab2ea1885ca14870bd8a1074cfc3ecaec9ff
Sean Beagle <30675442+SeanBeagle@users.noreply.github.com>
2021-02-09 20:23:02 -0500

    Update led.cpp

--- led.cpp
+++ led.cpp
@@ -3,71 +3,76 @@
 
 
 // HEADER
-enum Color { kRed, kYellow, kGreen };
-const std::string kColors[] { "red", "yellow", "green" };
-enum Mode { kOff, kSolid, kBlink, kAlternate };
-const std::string kModes[] { "off", "blink", "alternate" };
-
 class LED {
-  private:
-    // Fields
-    int _color;
-    int _mode;
-    // Methods
-    void SetColor(Color color);
-    void SetMode(Mode mode);
-    
   public:
-    // Constructors
+    enum Color {RED, YELLOW, GREEN};
+    enum Mode {OFF, SOLID, BLINK, ALTERNATE};
+    enum Rate {SLOW, MEDIUM, FAST};
+    // constructors
     LED();
     LED(Color color, Mode mode);
-    // Methods
-    void Blink(Color color);
+    // methods
+    void Blink(Color color, Rate rate);
+    void Alternate(Color color1, Color color2);
     void Solid(Color color);
     void Off();
     std::string ToString();
     
+  private:
+    Color _color1;
+    Color _color2;
+    Mode _mode;
+    Rate _rate;
+    // methods
+    void SetColor1(Color color);
+    void SetColor2(Color color);
+    void SetMode(Mode mode);
+    void SetRate(Rate rate);
 };
 
 
 // MAIN
 int main() {
    LED power_led = LED();
-   std::cout << power_led.ToString() << std::endl;
-   power_led.Solid(kGreen);
-   std::cout << power_led.ToString() << std::endl;
-   power_led.Solid(kRed);
-   std::cout << power_led.ToString() << std::endl;
+   power_led.Solid(LED::GREEN);
+   power_led.Solid(LED::RED);
+   power_led.Blink(LED::GREEN, LED::MEDIUM);
    power_led.Off();
-   std::cout << power_led.ToString() << std::endl;
+   power_led.Alternate(LED::RED, LED::YELLOW);
    
    return 0;
 }
 
 
-// DEFINITIONS
-
 /** Default constructor for LED */
 LED::LED() {
-    SetColor(kRed);
-    SetMode(kOff);
+    SetColor1(RED);
+    SetMode(OFF);
 }
 
 /** Construct LED with color and mode */
 LED::LED(Color color, Mode mode) {
-    SetColor(color);
+    SetColor1(color);
     SetMode(mode);
 }
 
-/** Set color of LED to kRed, kYellow, or kGreen */
-void LED::SetColor(Color color) {
-    if (_color != color) {
-        _color = color;
-        std::cout << "Set color to: " + kColors[_color] << std::endl;
+/** Set color1 of LED to kRed, kYellow, or kGreen */
+void LED::SetColor1(Color color) {
+    if (_color1 != color) {
+        _color1 = color;
+        std::cout << "Set color1 to: " + kColors[_color1] << std::endl;
     }
 }
 
-/** Set mode of LED to kOff, kSolid, or kAlternate */
+/** Set color2 of LED */
+void LED::SetColor2(Color color) {
+    if (_color2 != color) {
+        _color2 = color;
+        std::cout << "Set color2 to: " + kColors[_color2] << std::endl;
+    }
+}
+
+/** Set mode of LED */
 void LED::SetMode(Mode mode) {
     if (_mode != mode) {
         _mode = mode;
@@ -75,22 +80,37 @@ void LED::SetMode(Mode mode) {
     }
 }
 
-void LED::Solid(Color color) {
-    SetMode(kSolid);
-    SetColor(color);
+/** Set blink rate of LED */
+void LED::SetRate(Rate rate) {
+    if (_rate != rate) {
+        _rate = rate;
+        std::cout << "Set blink rate to: " + Rates[_rate] << std::endl;
+    }
 }
 
-void LED::Blink(Color color) {
-    SetMode(kBlink);
-    SetColor(color);
+void LED::Solid(Color color) {
+    SetMode(SOLID);
+    SetColor1(color);
+}
+
+void LED::Blink(Color color, Rate rate) {
+    SetMode(BLINK);
+    SetRate(rate);
+    SetColor1(color);
+}
+
+void LED::Alternate(Color color1, Color color2) {
+    SetMode(ALTERNATE);
+    SetColor1(color1);
+    SetColor2(color2);
 }
 
 void LED::Off() {
-    SetMode(kOff);
+    SetMode(OFF);
 }
 
 /** Return representation of object as string */
 std::string LED::ToString() {
-    return "LED<color=" + kColors[_color] + ", mode=" + kModes[_mode] + ">";
+    return "LED<color=" + kColors[_color1] + ", mode=" + kModes[_mode] + ">";
 }
 
